20969882d5d4ae0ce28badc30ce5f69af00c2605
Emre Durak <69764150+emredurak01@users.noreply.github.com>
2025-07-22 15:15:02 +0200

    kb layout menu case insensitive

--- .config/i3/kb_layout_switcher.sh
+++ .config/i3/kb_layout_switcher.sh
@@ -4,7 +4,7 @@ LANG_FILE="$HOME/.config/i3/layouts.txt"
 
 mapfile -t options < <(tail -n +2 "$LANG_FILE" | cut -d',' -f1)
 
-selected_index=$(printf "%s\n" "${options[@]}" | rofi -dmenu -p "Select layout" -format i)
+selected_index=$(printf "%s\n" "${options[@]}" | rofi -dmenu -p "Select layout" -format i -i)
 
 if [[ "$selected_index" =~ ^[0-9]+$ ]]; then
     layout_code=$(tail -n +2 "$LANG_FILE" | sed -n "$((selected_index + 1))p" | cut -d',' -f2)
