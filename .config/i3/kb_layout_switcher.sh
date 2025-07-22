6c29c7ae307c0618296b182f528368535c61554f
Emre Durak <69764150+emredurak01@users.noreply.github.com>
2025-07-22 15:09:32 +0200

    rofi kb layout switcher

---
+++ .config/i3/kb_layout_switcher.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+LANG_FILE="$HOME/.config/i3/layouts.txt"
+
+mapfile -t options < <(tail -n +2 "$LANG_FILE" | cut -d',' -f1)
+
+selected_index=$(printf "%s\n" "${options[@]}" | rofi -dmenu -p "Select layout" -format i)
+
+if [[ "$selected_index" =~ ^[0-9]+$ ]]; then
+    layout_code=$(tail -n +2 "$LANG_FILE" | sed -n "$((selected_index + 1))p" | cut -d',' -f2)
+    setxkbmap "$layout_code"
+fi
