cccb089c0ff52639187efe2b22c5c10c158b282a
Emre Durak <69764150+emredurak01@users.noreply.github.com>
2025-10-29 15:48:06 +0100

    nvim binding for $ and ^

--- .config/nvim/lua/mappings.lua
+++ .config/nvim/lua/mappings.lua
@@ -7,4 +7,7 @@ local map = vim.keymap.set
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 
+map("n", "E", "$", { desc = "Go to end of line"})
+map("n", "B", "^", { desc = "Go to first non-blank character" })
+
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
